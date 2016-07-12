ea65f0f6d6e8bff22dfd6c0ea17cbb2c492f6945
Brian Heim <bheim67@gmail.com>
2016-07-11 19:33:09 -0600

    Iteration 2 mod and test

---
+++ test/three_lines_test.rb
@@ -0,0 +1,14 @@
+gem 'minitest', '~> 5.0'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative '../lib/three_lines'
+
+class ThreeLineTest < Minitest::Test
+
+  def test_it_outputs_input_three_times
+    lines = ThreeLines.new("word")
+    assert_equal "#{"word"}\n#{"word"}\n#{"word"}", lines.change_to_three
+  end
+
+end
